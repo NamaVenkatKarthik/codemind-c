6688b24f2f57aea86f56c0d1c59b6377872769bf
Namakarthik143 <119730128+Namakarthik143@users.noreply.github.com>
2022-12-16 14:33:20 +0530

    #include<stdio.h> int main() {    int Number, Temp, Reminder, Rev = 0;    scanf("%d", &Number);
       Temp = Number;
    
       while ( Temp > 0)
       {
          Reminder = Temp %10;
          Rev = Rev *10+ Reminder;
           Temp = Temp /10;
       }
       printf("%d",Rev);
    }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int Number, Temp, Reminder, Rev = 0;
+   scanf("%d", &Number);
+  
+   Temp = Number;
+
+   while ( Temp > 0)
+   {
+      Reminder = Temp %10;
+      Rev = Rev *10+ Reminder;
+       Temp = Temp /10;
+   }
+   printf("%d",Rev);
+}
