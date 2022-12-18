9f772f365f5913e9402efa46c822358b44077b6f
Namakarthik143 <119730128+Namakarthik143@users.noreply.github.com>
2022-12-18 11:16:05 +0530

    #include<stdio.h> int main() {     float gross,da,hra,basic;     scanf("%f",&basic);     if(basic<=10000)     {         da=basic*0.8;         hra=basic*0.2;     }     else if(basic<=20000)     {         da=basic*0.9;         hra=basic*0.25;     }     else     {         da=basic*0.95;         hra=basic*0.3;     }     gross=basic+da+hra;     printf("%0.2f",gross); }

---
+++ Gross_Salary.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    float gross,da,hra,basic;
+    scanf("%f",&basic);
+    if(basic<=10000)
+    {
+        da=basic*0.8;
+        hra=basic*0.2;
+    }
+    else if(basic<=20000)
+    {
+        da=basic*0.9;
+        hra=basic*0.25;
+    }
+    else
+    {
+        da=basic*0.95;
+        hra=basic*0.3;
+    }
+    gross=basic+da+hra;
+    printf("%0.2f",gross);
+}
