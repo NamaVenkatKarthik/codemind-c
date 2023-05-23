f1bed05132536200ae176476aa620571eb36afd1
Nama Venkat Karthik <119730128+Namakarthik143@users.noreply.github.com>
2023-05-23 21:27:16 +0530

    #include<stdio.h> int main(){     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--){         for(j=1;j<=i;j++){             printf("%c ",(char)(i+64));         }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
-int main()
-{
+int main(){
     int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=i;j++)
-        {
-            printf("%d",j);
+    for(i=n;i>=1;i--){
+        for(j=1;j<=i;j++){
+            printf("%c ",(char)(i+64));
         }
         printf("
 ");
     }
+    
 }
