52fb589bc1d03adfcae17cb31a7a54dced740023
Nama Venkat Karthik <119730128+NamaVenkatKarthik@users.noreply.github.com>
2024-01-13 20:13:53 +0530

    #include <stdio.h>
    int main() {
        float base, height, area;
        scanf("%f", &base);
        scanf("%f", &height);
        area = (base * height) / 2;
        printf("%.f
    ", area);
    }

---
+++ Area_of_a_triangle_.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float base, height, area;
+    scanf("%f", &base);
+    scanf("%f", &height);
+    area = (base * height) / 2;
+    printf("%.f
+", area);
+}
